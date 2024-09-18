7450699048c41f39b80c53aab0152a2e0e0bbe58
RedPug <therowanrichards@gmail.com>
2024-09-17 20:51:34 -0700

    Added Notes Menu

---
+++ Slide Rule/Notes/MathNotesView.swift
@@ -0,0 +1,18 @@
+//
+//  MathNotesView.swift
+//  Slide Rule
+//
+//  Created by Rowan on 9/12/24.
+//
+
+import SwiftUI
+
+struct MathNotesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MathNotesView()
+}
