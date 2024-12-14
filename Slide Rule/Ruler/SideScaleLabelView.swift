faddb1d66b41a9ef6f6be1cd85a6cd506b4a2024
RedPug <therowanrichards@gmail.com>
2024-12-14 13:34:32 -0800

    basic equation tutorials

---
+++ Slide Rule/Ruler/SideScaleLabelView.swift
@@ -0,0 +1,18 @@
+//
+//  SideScaleLabelView.swift
+//  Slide Rule
+//
+//  Created by Rowan on 11/28/24.
+//
+
+import SwiftUI
+
+struct SideScaleLabelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideScaleLabelView()
+}
---
+++ Slide Rule/Tutorial/Equation Parser/ValueRange.swift
@@ -0,0 +1,8 @@
+//
+//  ValueRansge.swift
+//  Slide Rule
+//
+//  Created by Rowan on 11/26/24.
+//
+
+import Foundation
